49997b95d9d5bf58ffccc585003d2b0de039d504
Tiago Martinho <t.martinho@icloud.com>
2018-11-15 15:04:33 +0000

    Add table view container

--- Gulps/ViewControllers/ExerciseViewController.swift
+++ Gulps/ViewControllers/ExerciseViewController.swift
@@ -22,6 +22,26 @@ class ExerciseViewController: UIViewController {
     title = "Exercises"
     navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismissView))
     addCompleteButton()
+    addExercisesTable()
+  }
+
+  private func addExercisesTable() {
+    let tableView = UIView()
+    tableView.backgroundColor = .red
+    view.addSubview(tableView)
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    let item: Any
+    if #available(iOS 11.0, *) {
+      item = view.safeAreaLayoutGuide
+    } else {
+      item = view
+    }
+    let top = NSLayoutConstraint(item: tableView, attribute: .top, relatedBy: .equal, toItem: item, attribute: .top, multiplier: 1, constant: 12)
+    let bottom = NSLayoutConstraint(item: tableView, attribute: .bottom, relatedBy: .equal, toItem: completeButton, attribute: .top, multiplier: 1, constant: -12)
+    let trailing = NSLayoutConstraint(item: tableView, attribute: .trailing, relatedBy: .equal, toItem: item, attribute: .trailing, multiplier: 1, constant: -12)
+    let leading = NSLayoutConstraint(item: tableView, attribute: .leading, relatedBy: .equal, toItem: item, attribute: .leading, multiplier: 1, constant: 12)
+    let constraints = [top, bottom, trailing, leading]
+    NSLayoutConstraint.activate(constraints)
   }
 
   private func addCompleteButton() {
