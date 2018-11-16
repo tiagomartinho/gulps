d41caf230cb6191d0e03d2a604c82e330740e0a7
Tiago Martinho <t.martinho@icloud.com>
2018-11-16 16:57:27 +0000

    Add Exercise Cell

--- Gulps/ViewControllers/ExercisesTableViewController.swift
+++ Gulps/ViewControllers/ExercisesTableViewController.swift
@@ -1,6 +1,68 @@
 import UIKit
+import TinyConstraints
+
+struct Exercise {
+  let title: String
+  let description: String
+  let image: UIImage
+}
 
 class ExercisesTableViewCell: UITableViewCell {
+
+  var exercise: Exercise? {
+    didSet {
+      exerciseImageView.image = exercise?.image
+      titleLabel.text = exercise?.title
+      descriptionLabel.text = exercise?.description
+    }
+  }
+
+  private let stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.spacing = 12
+    stackView.distribution = .fill
+    stackView.alignment = .fill
+    return stackView
+  }()
+
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.preferredFont(forTextStyle: .title1)
+    label.textAlignment = .center
+    return label
+  }()
+
+
+  private let descriptionLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.preferredFont(forTextStyle: .callout)
+    label.textAlignment = .center
+    label.numberOfLines = 0
+    return label
+  }()
+
+  private let exerciseImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFit
+    imageView.clipsToBounds = true
+    imageView.height(200)
+    return imageView
+  }()
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    stackView.addArrangedSubview(titleLabel)
+    stackView.addArrangedSubview(exerciseImageView)
+    stackView.addArrangedSubview(descriptionLabel)
+    addSubview(stackView)
+    let insets = UIEdgeInsets(top: 12, left: 8, bottom: -12, right: -8)
+    stackView.edges(to: self, insets: insets)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
 
 class ExercisesTableViewController: UITableViewController {
@@ -17,10 +79,9 @@ class ExercisesTableViewController: UITableViewController {
   }
 
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier)
-    let newCell = UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
-    let cell = dequeuedCell ?? newCell
-    cell.textLabel?.text = "\(indexPath.row)"
+    let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) as? ExercisesTableViewCell
+    let cell = dequeuedCell ?? ExercisesTableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
+    cell.exercise = Exercise(title: "Goldfish", description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", image: UIImage(named: "open")!)
     return cell
   }
 }
