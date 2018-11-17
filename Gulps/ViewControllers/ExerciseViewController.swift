f5a1a738d810e103cd9e7e90e8b2f538d76d90dd
Tiago Martinho <t.martinho@icloud.com>
2018-11-17 12:10:08 +0000

    Fix complete button constraints

--- Gulps/ViewControllers/ExerciseViewController.swift
+++ Gulps/ViewControllers/ExerciseViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import TinyConstraints
 
 class ExerciseViewController: UIViewController {
 
@@ -29,20 +30,13 @@ class ExerciseViewController: UIViewController {
     let tableView = UIView()
     tableView.backgroundColor = .red
     view.addSubview(tableView)
-    tableView.translatesAutoresizingMaskIntoConstraints = false
-    let item: Any
-    if #available(iOS 11.0, *) {
-      item = view.safeAreaLayoutGuide
-    } else {
-      item = view
-    }
-    let top = NSLayoutConstraint(item: tableView, attribute: .top, relatedBy: .equal, toItem: item, attribute: .top, multiplier: 1, constant: 0)
-    let bottom = NSLayoutConstraint(item: tableView, attribute: .bottom, relatedBy: .equal, toItem: completeButton, attribute: .top, multiplier: 1, constant: 0)
-    let trailing = NSLayoutConstraint(item: tableView, attribute: .trailing, relatedBy: .equal, toItem: item, attribute: .trailing, multiplier: 1, constant: -12)
-    let leading = NSLayoutConstraint(item: tableView, attribute: .leading, relatedBy: .equal, toItem: item, attribute: .leading, multiplier: 1, constant: 12)
-    let constraints = [top, bottom, trailing, leading]
-    NSLayoutConstraint.activate(constraints)
     let tableViewController = ExercisesTableViewController()
+    if #available(iOS 10.0, *) {
+      tableView.edgesToSuperview(excluding: .bottom, insets: .uniform(8), usingSafeArea: true)
+    } else {
+      tableView.edges(to: view, excluding: .bottom, insets: .uniform(8))
+    }
+    tableView.bottomToTop(of: completeButton)
     addChild(tableViewController)
     tableViewController.view.frame = tableView.frame
     tableView.addSubview(tableViewController.view)
@@ -51,19 +45,12 @@ class ExerciseViewController: UIViewController {
 
   private func addCompleteButton() {
     view.addSubview(completeButton)
-    completeButton.translatesAutoresizingMaskIntoConstraints = false
-    let item: Any
-    if #available(iOS 11.0, *) {
-      item = view.safeAreaLayoutGuide
+    if #available(iOS 10.0, *) {
+      completeButton.edgesToSuperview(excluding: .top, insets: .uniform(8), usingSafeArea: true)
     } else {
-      item = view
+      completeButton.edges(to: view, excluding: .top, insets: .uniform(8))
     }
-    let height = NSLayoutConstraint(item: completeButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 60)
-    let bottom = NSLayoutConstraint(item: completeButton, attribute: .bottom, relatedBy: .equal, toItem: item, attribute: .bottom, multiplier: 1, constant: 0)
-    let trailing = NSLayoutConstraint(item: completeButton, attribute: .trailing, relatedBy: .equal, toItem: item, attribute: .trailing, multiplier: 1, constant: -20)
-    let leading = NSLayoutConstraint(item: completeButton, attribute: .leading, relatedBy: .equal, toItem: item, attribute: .leading, multiplier: 1, constant: 20)
-    let constraints = [height, bottom, trailing, leading]
-    NSLayoutConstraint.activate(constraints)
+    completeButton.height(60)
   }
 
   @objc func dismissView() {
