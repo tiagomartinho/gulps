b94cfceccf0a54ffb73820afe9b11f4a8d8cd8a6
Tiago Martinho <t.martinho@icloud.com>
2018-11-19 15:13:44 +0100

    Set notifications off by default

--- Gulps/Support/Settings.swift
+++ Gulps/Support/Settings.swift
@@ -7,7 +7,7 @@ open class Settings {
       userDefaults.set(true, forKey: "DEFAULTS_INSTALLED")
       userDefaults.set(1, forKey: Constants.Gulp.small.key())
       userDefaults.set(6, forKey: Constants.Gulp.goal.key())
-      userDefaults.set(true, forKey: Constants.Notification.on.key())
+      userDefaults.set(false, forKey: Constants.Notification.on.key())
       userDefaults.set(10, forKey: Constants.Notification.from.key())
       userDefaults.set(22, forKey: Constants.Notification.to.key())
       userDefaults.set(2, forKey: Constants.Notification.interval.key())
